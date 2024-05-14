6e2f3d09e91c16394fa6a78791ba010fa52d1e77
Anish jha <122715402+anish-stack@users.noreply.github.com>
2024-05-14 14:12:50 +0530

    payment gateway

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,6 +1,6 @@
 const Orders = require('../models/OrderModel');
-const merchantId = "PGTESTPAYUAT"
-const apiKey = "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399"
+const merchantId = "M2257T8PKCFTS"
+const apiKey = "382a6ef0-8a78-4abd-bc79-5fd4afca18e6"
 const crypto = require('crypto');
 const axios = require('axios');
 const { search } = require('../routes/routes');
@@ -26,7 +26,7 @@ async function doPayment(amount, Merchant, transactionId, res, req) {
         const string = payloadMain + '/pg/v1/pay' + apiKey;
         const sha256 = crypto.createHash('sha256').update(string).digest('hex');
         const checksum = sha256 + '###' + keyIndex;
-        const prod_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+        const prod_URL = "https://api.phonepe.com/apis/hermes";
         // console.log(checksum)
         const options = {
             method: 'POST',
@@ -145,8 +145,8 @@ exports.checkStatus = async (req, res) => {
     const string = `/pg/v1/status/${merchantId}/${merchantTransactionId}` + apiKey;
     const sha256 = crypto.createHash('sha256').update(string).digest('hex');
     const checksum = sha256 + "###" + keyIndex;
-    const testUrlCheck = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1"
-    // const testUrlCheck = "https://api.phonepe.com/apis/hermes/pg/v1/"
+    // const testUrlCheck = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1"
+    const testUrlCheck = "https://api.phonepe.com/apis/hermes/pg/v1/"
 
     // Prepare the options for the HTTP request
     const options = {
